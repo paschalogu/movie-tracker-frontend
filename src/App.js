1fea1cc2afe7f6e0d5d65f93f45604e2c891f7da
MazeBuer <mazikeenbuer@gmail.com>
2022-07-01 18:34:55 -0700

    API error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react' ;
+import React, { useState, useEffect } from 'react';
 import { CssBaseline, Grid } from '@material-ui/core';
 
 import { getPlacesData, getWeatherData } from './api/index';
@@ -6,92 +6,88 @@ import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 
-
 const App = () => {
-    const [type, setType] = useState('restaurants');
-    const [rating, setRating] = useState('');
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
 
-    const [places, setPlaces] = useState([]);
-    const [weatherData, setWeatherData] = useState([]);
-    const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [coords, setCoords] = useState({});
+  const [bounds, setBounds] = useState(null); //
 
-    const [childClicked, setChildClicked] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const [autocomplete, setAutocomplete] = useState(null);
+  const [weatherData, setWeatherData] = useState([]);
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [places, setPlaces] = useState([]);
 
-    const [coords, setCoords] = useState( {}); //coords
-    const [bounds, setBounds] = useState({});
+  const [autocomplete, setAutocomplete] = useState(null);
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+      setCoords({ lat: latitude, lng: longitude });
+    });
+  }, []);
 
+  useEffect(() => {
+    const filtered = places.filter((place) => Number(place.rating) > rating);
 
-    useEffect( () => {
-        navigator.geolocation.getCurrentPosition( ({ coords: {latitude, longitude}}) => {
-            setCoords({ lat: latitude, lng: longitude}) //change setCoordinates
+    setFilteredPlaces(filtered);
+  }, [rating]);
 
-        })
-    },[])
+  useEffect(() => {
+    if (bounds) {
+      setIsLoading(true);
 
-    useEffect( () => {
-        const filtered = places.filter((place) => Number(place.rating) > rating);
+      getWeatherData(coords.lat, coords.lng)
+        .then((data) => setWeatherData(data));
 
-        setFilteredPlaces(filtered);
+      getPlacesData(type, bounds.sw, bounds.ne)
+        .then((data) => {
+          setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
+          setFilteredPlaces([]);
+          setRating('');
+          setIsLoading(false);
+        });
+    }
+  }, [bounds, type]);
 
-    }, [rating]);
+  const onLoad = (autoC) => setAutocomplete(autoC);
 
-    useEffect( () => {
-        if(bounds.sw && bounds.ne) {
-        setIsLoading(true);
-        getWeatherData(coords.lat, coords.lng)
-            .then( (data) => setWeatherData(data));
-
-        getPlacesData(type, bounds.sw, bounds.ne)
-            .then( (data) => {
-                setPlaces(data?.filter( (place) => place.name && place.num_reviews > 0));
-                setFilteredPlaces([]);
-                setRating('');
-                setIsLoading(false);
-            })
-        }
-    }, [type, bounds]);
-
-    const onLoad = (autoC) => setAutocomplete(autoC);
-
-    const onPlaceChanged = () => {
+  const onPlaceChanged = () => {
     const lat = autocomplete.getPlace().geometry.location.lat();
     const lng = autocomplete.getPlace().geometry.location.lng();
 
     setCoords({ lat, lng });
   };
 
-    return (
-        <>
-            <CssBaseline />    
-            <Header onPlaceChanged={onPlaceChanged} onLoad={onLoad} /> {/*This is the Header.jsx file; how big it will be on the pag and position */}
-            <Grid container spacing={3} style={{width: '100%'}}>
-                <Grid item xs={12} md={4}>
-                    <List 
-                        places={filteredPlaces.length ? filteredPlaces : places}
-                        childClicked={childClicked}//HERE I CHANGED FROM CHILDCLICKED TO ONCHILDCLICKED
-                        isLoading={isLoading}
-                        type={type}
-                        setType={setType}
-                        rating={rating}
-                        setRating={setRating}
-                    /> {/*The List.jsx file inside of this grid with size of __  */}
-                </Grid>
-                <Grid item xs={12} md={8} style={{ display: 'flex', justifyContent:'center', alignItems: 'center'}}>
-                    <Map 
-                        setCoords={setCoords}
-                        setBounds={setBounds}
-                        coords = {coords}
-                        places={filteredPlaces.length ? filteredPlaces : places}
-                        setChildClicked={setChildClicked}
-                        weatherData={weatherData}
-                    /> {/*The Map.jsx file inside of this grid with size of __  */}
-                </Grid>
-            </Grid>
-        </>
-    );
-}
+  return (
+    <>
+      <CssBaseline />
+      <Header onPlaceChanged={onPlaceChanged} onLoad={onLoad} />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List
+            isLoading={isLoading}
+            childClicked={childClicked}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
+        </Grid>
+        <Grid item xs={12} md={8} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Map
+            setChildClicked={setChildClicked}
+            setBounds={setBounds}
+            setCoords={setCoords}
+            coords={coords}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            weatherData={weatherData}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
 export default App;
--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 export const getPlacesData = async (type, sw, ne) => {
     try{
         const { data: {data}} = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+            withCredentials:true,
             params: {
               bl_latitude: sw.lat,
               tr_latitude: ne.lat,
@@ -12,11 +13,16 @@ export const getPlacesData = async (type, sw, ne) => {
               tr_longitude: ne.lng,
             },
             headers: {
+              "Content-Type": 'text/plain',
               'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_API_KEY,
               'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
             }
           } );
-
+          axios.request(options).then(function (response) {
+            console.log(response.data);
+          }).catch(function (error) {
+            console.error(error);
+          });
         return data;
     }
     catch (error) {
@@ -28,7 +34,7 @@ export const getPlacesData = async (type, sw, ne) => {
 export const getWeatherData = async (lat, lng) => {
   try {
     if(lat && lng) {
-      const {data} = await axios.get('https://community-open-weather-map.p.rapidapi.com/weather', {
+      const {data} = await axios.get('https://open-weather13.p.rapidapi.com/city/landon', {
         params: {
         lat: lat,
         lon: lng,
@@ -38,8 +44,10 @@ export const getWeatherData = async (lat, lng) => {
         'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com'
         }
       });
+      console.log(data)
+      return data;
     }
-  return data;
+  
   }
   catch(error) {
     console.log(error)
